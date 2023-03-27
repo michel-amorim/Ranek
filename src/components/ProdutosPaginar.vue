f4a0ac31e2535c20ceff3104f6acbc99004ea8c3
michel-amorim <michelmsa7799@gmail.com>
2023-03-27 14:57:12 -0300

    paginacao

--- src/components/ProdutosPaginar.vue
+++ src/components/ProdutosPaginar.vue
@@ -1,7 +1,7 @@
 <template>
   <ul v-if="paginasTotal > 1">
     <li v-for="pagina in paginasTotal" :key="pagina">
-      <router-link :to="{query: query(pagina)}">{{pagina}}</router-link>
+      <router-link :to="{ query: query(pagina) }">{{ pagina }}</router-link>
     </li>
   </ul>
 </template>
@@ -11,27 +11,43 @@ export default {
   props: {
     produtosPorPagina: {
       type: Number,
-      default: 1
+      default: 1,
     },
     produtosTotal: {
       type: Number,
-      default: 1
-    }
+      default: 1,
+    },
   },
   methods: {
     query(pagina) {
       return {
         ...this.$route.query,
-        _page: pagina
+        _page: pagina,
       };
-    }
+    },
   },
   computed: {
+    paginas() {
+      const current = Number(this.$route.query._page);
+      const range = 9;
+      const offset = Math.ceil(range / 2);
+      const total = this.paginasTotal;
+      const pagesArray = [];
+
+      for (let i = 1; i <= total; i++) {
+        pagesArray.push(i);
+      }
+
+      pagesArray.splice(0, current - offset);
+      pagesArray.splice(range, total);
+
+      return pagesArray;
+    },
     paginasTotal() {
       const total = this.produtosTotal / this.produtosPorPagina;
       return total !== Infinity ? Math.ceil(total) : 0;
-    }
-  }
+    },
+  },
 };
 </script>
 
